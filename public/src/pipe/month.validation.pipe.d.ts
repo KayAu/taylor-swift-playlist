8ae34b6bbf09e69df519fe4f7d12d8778f6529a4
KayAu <kayeau80@gmail.com>
2024-08-18 22:55:38 +0800

    updated package json

--- public/src/pipe/month.validation.pipe.d.ts
+++
@@ -1,5 +0,0 @@
-import { PipeTransform } from '@nestjs/common';
-export declare class MonthValidationPipe implements PipeTransform {
-    private readonly months;
-    transform(value: string): string;
-}
--- public/src/pipe/sortBy.validation.pipe.d.ts
+++
@@ -1,5 +0,0 @@
-import { PipeTransform } from '@nestjs/common';
-export declare class SortByValidationPipe implements PipeTransform {
-    private readonly validSortFields;
-    transform(value: string): string;
-}
