cca895f5a43d977d28a1dba7c3dcd05af1dca459
KayAu <kayeau80@gmail.com>
2024-08-20 16:00:32 +0800

    Add files via upload

---
+++ taylor-swift-playlist_git_clone.bat
@@ -0,0 +1,9 @@
+git clone https://github.com/KayAu/taylor-swift-playlist.git
+
+cd taylor-swift-playlist
+
+npm install --force
+
+echo "taylor-swift-playlist project has been successfully checked out."
+
+pause
